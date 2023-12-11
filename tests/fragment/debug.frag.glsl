6c61bd77fcb83c8da37d95d6c6d524af39d45f64
Mary Guillemard <mary@mary.zone>
2023-12-11 17:56:56 +0100

    more testing stuffs

--- tests/fragment/debug.frag.glsl
+++ tests/fragment/debug.frag.glsl
@@ -1,9 +1,38 @@
 #version 450
+#extension GL_EXT_mesh_shader : enable
+#extension GL_EXT_shader_8bit_storage: require
 
-layout(location = 0) in vec4 o_color;
-layout(location = 0) out vec4 uFragColor;
+layout (location=0) in vec4 customAttribute1;
+layout (location=1) in flat float customAttribute2;
+layout (location=2) in flat int customAttribute3;
 
-void main(void)
+layout (location=3) in perprimitiveEXT flat uvec4 customAttribute4;
+layout (location=4) in perprimitiveEXT float customAttribute5;
+
+layout (location=0) out vec4 outColor;
+
+layout(binding = 0) buffer UniformBufferObject {
+    uint data[0x1000];
+} ubo;
+
+void main ()
 {
-    uFragColor = o_color;
+    bool goodPrimitiveID = (gl_PrimitiveID == 1000 || gl_PrimitiveID == 1001);
+    bool goodViewportIndex = (gl_ViewportIndex == 1);
+    bool goodCustom1 = (customAttribute1.x >= 0.25 && customAttribute1.x <= 0.5 &&
+                        customAttribute1.y >= 0.5  && customAttribute1.y <= 1.0 &&
+                        customAttribute1.z >= 10.0 && customAttribute1.z <= 20.0 &&
+                        customAttribute1.w == 3.0);
+    bool goodCustom2 = (customAttribute2 == 1.0 || customAttribute2 == 2.0);
+    bool goodCustom3 = (customAttribute3 == 3 || customAttribute3 == 4);
+    bool goodCustom4 = ((gl_PrimitiveID == 1000 && customAttribute4 == uvec4(100, 101, 102, 103)) ||
+                        (gl_PrimitiveID == 1001 && customAttribute4 == uvec4(200, 201, 202, 203)));
+    bool goodCustom5 = ((gl_PrimitiveID == 1000 && customAttribute5 == 6.0) ||
+                        (gl_PrimitiveID == 1001 && customAttribute5 == 7.0));
+    
+    if (goodPrimitiveID && goodViewportIndex && goodCustom1 && goodCustom2 && goodCustom3 && goodCustom4 && goodCustom5) {
+        outColor = vec4(0.0, 0.0, 1.0, 1.0);
+    } else {
+        outColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
 }
--- tests/mesh/simple.mesh copy.glsl
+++ tests/mesh/simple.mesh copy.glsl
@@ -1,8 +1,8 @@
 #version 450
 #extension GL_EXT_mesh_shader : require
 
-layout(local_size_x = 2, local_size_y = 1, local_size_z = 1) in;
-layout(lines, max_vertices = 256, max_primitives = 256) out;
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+layout(triangles, max_vertices = 3, max_primitives = 1) out;
 
 layout(location = 0) out mesh_output
 {
@@ -23,26 +23,19 @@ const vec4[3] c_colors = {
 
 void main()
 {
-    // Define two lines.
-	SetMeshOutputsEXT(2, 2);
+    // Define one triangle.
+	SetMeshOutputsEXT(3, 1);
 
     // Set position of each vertice.
 	gl_MeshVerticesEXT[0].gl_Position = c_positions[0];
 	gl_MeshVerticesEXT[1].gl_Position = c_positions[1];
-
-	gl_MeshVerticesEXT[2].gl_Position = c_positions[1];
-	gl_MeshVerticesEXT[3].gl_Position = c_positions[2];
-
+	gl_MeshVerticesEXT[2].gl_Position = c_positions[2];
 
     // Set associated to each vertice.
 	o_mesh_output[0].color = c_colors[0];
 	o_mesh_output[1].color = c_colors[1];
-
-	o_mesh_output[2].color = c_colors[1];
-	o_mesh_output[3].color = c_colors[2];
-
+	o_mesh_output[2].color = c_colors[2];
 
     // Finally set triangle primitive indices.
-	gl_PrimitiveLineIndicesEXT[0] =  uvec2(0, 1);
-	gl_PrimitiveLineIndicesEXT[1] =  uvec2(2, 3);
+	gl_PrimitiveTriangleIndicesEXT[gl_LocalInvocationIndex] =  uvec3(0, 1, 2);
 }
