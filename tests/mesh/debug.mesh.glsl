65caf42c0e940584c4f8ec6e6a8d665aed8fb9a3
Mary Guillemard <mary@mary.zone>
2023-12-18 17:36:54 +0100

    More tests

--- tests/mesh/debug.mesh.glsl
+++ tests/mesh/debug.mesh.glsl
@@ -1,62 +1,23 @@
 #version 450
 #extension GL_EXT_mesh_shader : enable
 
-const uint invocationCount = 128;
+const uint workGroupSize = 4;
+const uint taskInvocationCount = 1 * workGroupSize;
+const uint meshInvocationCount = 1 * workGroupSize;
 
-layout (local_size_x=invocationCount, local_size_y=1, local_size_z=1) in;
+layout (local_size_x=meshInvocationCount, local_size_y=1, local_size_z=1) in;
 layout (triangles) out;
 layout (max_vertices=3, max_primitives=1) out;
 const uint payloadElements = 1u;
 
-// 28672
-const uint sharedMemoryElements = 28672 / 4;
-
 layout(binding = 0) buffer UniformBufferObject {
-    uint sharedOK;
-    uint sharedMemoryErrorIndex;
-    uint sharedMemoryErrorValue;
-    uint sharedMemoryErrorExpected;
-    uint localInvocationCount[invocationCount];
+    uint taskInvocations[taskInvocationCount];
+    uint meshInvocations[meshInvocationCount];
 } result;
 
-shared uint sharedElements[sharedMemoryElements];
-
 void main () {
-    const uint shMemElementsPerInvocation = uint(ceil(float(sharedMemoryElements) / float(invocationCount)));
-    for (uint i = 0u; i < shMemElementsPerInvocation; ++i) {
-        const uint elemIdx = shMemElementsPerInvocation * gl_LocalInvocationIndex + i;
-        if (elemIdx < sharedMemoryElements) {
-            sharedElements[elemIdx] = elemIdx * 2u + 1000u;
-        }
-    }
-    memoryBarrierShared();
-    barrier();
-    for (uint i = 0u; i < shMemElementsPerInvocation; ++i) {
-        const uint elemIdx = shMemElementsPerInvocation * gl_LocalInvocationIndex + i;
-        if (elemIdx < sharedMemoryElements) {
-            const uint accessIdx = sharedMemoryElements - 1u - elemIdx;
-            sharedElements[accessIdx] += accessIdx;
-
-            if (accessIdx == 1) {
-                result.localInvocationCount[gl_LocalInvocationIndex] = sharedElements[accessIdx];
-            }
-        }
-    }
-    memoryBarrierShared();
-    barrier();
-    if (gl_LocalInvocationIndex == 0u) {
-        bool allOK = true;
-        for (uint i = 0u; i < sharedMemoryElements; ++i) {
-            if (sharedElements[i] != i*3u + 1000u) {
-                allOK = false;
-                result.sharedMemoryErrorIndex = i;
-                result.sharedMemoryErrorValue = sharedElements[i];
-                result.sharedMemoryErrorExpected =  i*3u + 1000u;
-                break;
-            }
-        }
-        result.sharedOK = (allOK ? 1u : 0u);
-    }
+    const uint workGroupIndex = gl_NumWorkGroups.x * gl_NumWorkGroups.y * gl_WorkGroupID.z + gl_NumWorkGroups.x * gl_WorkGroupID.y + gl_WorkGroupID.x;
+    result.meshInvocations[gl_LocalInvocationIndex * workGroupIndex] = workGroupIndex + 1;
 
     SetMeshOutputsEXT(0u, 0u);
 }
--- tests/task/debug.task.glsl
+++ tests/task/debug.task.glsl
@@ -2,7 +2,19 @@
 
 #extension GL_EXT_mesh_shader : require
 
+const uint workGroupSize = 4;
+const uint taskInvocationCount = 1 * workGroupSize;
+const uint meshInvocationCount = 1 * workGroupSize;
+
+layout(binding = 0) buffer UniformBufferObject {
+    uint taskInvocations[taskInvocationCount];
+    uint meshInvocations[meshInvocationCount];
+} result;
+
 void main() 
 {
-	EmitMeshTasksEXT(3, 1, 1);
+    const uint workGroupIndex = gl_NumWorkGroups.x * gl_NumWorkGroups.y * gl_WorkGroupID.z + gl_NumWorkGroups.x * gl_WorkGroupID.y + gl_WorkGroupID.x;
+    result.taskInvocations[gl_LocalInvocationIndex * workGroupIndex] = workGroupIndex;
+
+	EmitMeshTasksEXT(1, 1, 1);
 }
