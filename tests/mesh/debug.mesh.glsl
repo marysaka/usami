5efba469067a1a555e6b56fada6e72569fbe952b
Mary Guillemard <mary@mary.zone>
2023-12-18 17:44:27 +0100

    fixup for last commit

--- tests/mesh/debug.mesh.glsl
+++ tests/mesh/debug.mesh.glsl
@@ -2,8 +2,8 @@
 #extension GL_EXT_mesh_shader : enable
 
 const uint workGroupSize = 4;
-const uint taskInvocationCount = 1 * workGroupSize;
-const uint meshInvocationCount = 1 * workGroupSize;
+const uint taskInvocationCount = 1;
+const uint meshInvocationCount = 1;
 
 layout (local_size_x=meshInvocationCount, local_size_y=1, local_size_z=1) in;
 layout (triangles) out;
@@ -11,13 +11,13 @@ layout (max_vertices=3, max_primitives=1) out;
 const uint payloadElements = 1u;
 
 layout(binding = 0) buffer UniformBufferObject {
-    uint taskInvocations[taskInvocationCount];
-    uint meshInvocations[meshInvocationCount];
+    uint taskInvocations[taskInvocationCount * workGroupSize];
+    uint meshInvocations[meshInvocationCount * workGroupSize];
 } result;
 
 void main () {
     const uint workGroupIndex = gl_NumWorkGroups.x * gl_NumWorkGroups.y * gl_WorkGroupID.z + gl_NumWorkGroups.x * gl_WorkGroupID.y + gl_WorkGroupID.x;
-    result.meshInvocations[gl_LocalInvocationIndex * workGroupIndex] = workGroupIndex + 1;
+    result.meshInvocations[gl_LocalInvocationIndex + workGroupIndex] = workGroupIndex;
 
     SetMeshOutputsEXT(0u, 0u);
 }
--- tests/task/debug.task.glsl
+++ tests/task/debug.task.glsl
@@ -3,18 +3,19 @@
 #extension GL_EXT_mesh_shader : require
 
 const uint workGroupSize = 4;
-const uint taskInvocationCount = 1 * workGroupSize;
-const uint meshInvocationCount = 1 * workGroupSize;
+const uint taskInvocationCount = 1;
+const uint meshInvocationCount = 1;
+layout (local_size_x=taskInvocationCount, local_size_y=1, local_size_z=1) in;
 
 layout(binding = 0) buffer UniformBufferObject {
-    uint taskInvocations[taskInvocationCount];
-    uint meshInvocations[meshInvocationCount];
+    uint taskInvocations[taskInvocationCount * workGroupSize];
+    uint meshInvocations[meshInvocationCount * workGroupSize];
 } result;
 
 void main() 
 {
     const uint workGroupIndex = gl_NumWorkGroups.x * gl_NumWorkGroups.y * gl_WorkGroupID.z + gl_NumWorkGroups.x * gl_WorkGroupID.y + gl_WorkGroupID.x;
-    result.taskInvocations[gl_LocalInvocationIndex * workGroupIndex] = workGroupIndex;
+    result.taskInvocations[gl_LocalInvocationIndex + workGroupIndex] = workGroupIndex;
 
 	EmitMeshTasksEXT(1, 1, 1);
 }
