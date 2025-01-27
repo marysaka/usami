92326d30b34b7e2142f98d4cccf6e403aa0920e5
Mary Guillemard <mary@mary.zone>
2025-01-27 11:54:47 +0100

    Add muladd tests to generator and regenerate everything
    This also uses constant values to make it easier to determine what
    values is placed where.
    
    Signed-off-by: Mary Guillemard <mary@mary.zone>

---
+++ coop_matrix_layout_store_shaders/sm86/16x16x16/matrix_muladd_float16_float16_float32_float32_16x16x16.glsl
@@ -0,0 +1,62 @@
+// matrix_muladd_float16_float16_float32_float32_16x16x16.glsl
+
+// compiler_extensions: VK_KHR_cooperative_matrix
+#version 450
+
+#extension GL_GOOGLE_include_directive : require
+#extension GL_EXT_shader_16bit_storage : require
+#extension GL_EXT_shader_explicit_arithmetic_types_float16 : require
+#extension GL_KHR_memory_scope_semantics : require
+#extension GL_EXT_shader_explicit_arithmetic_types : require
+#extension GL_KHR_shader_subgroup_basic : require
+#extension GL_KHR_cooperative_matrix : require
+
+#define elementTypeA float16_t
+#define elementTypeB float16_t
+#define elementTypeC float32_t
+#define elementTypeD float32_t
+const int M = 16;
+const int N = 16;
+const int K = 16;
+
+
+layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z = 1) in;
+layout(set=0, binding=0) buffer Output { elementTypeD x[]; } outputO;
+layout(set=0, binding=1) buffer CustomStride { uint stride[]; } customStride;
+layout(set=0, binding=3) buffer CustomElement { uint element[]; } customElement;
+
+#define coopmatTypeA coopmat<elementTypeA, gl_ScopeSubgroup, M, K, gl_MatrixUseA>
+#define coopmatTypeB coopmat<elementTypeB, gl_ScopeSubgroup, K, N, gl_MatrixUseB>
+#define coopmatTypeC coopmat<elementTypeC, gl_ScopeSubgroup, M, N, gl_MatrixUseAccumulator>
+#define coopmatTypeD coopmat<elementTypeD, gl_ScopeSubgroup, M, N, gl_MatrixUseAccumulator>
+
+void main()
+{
+   uint element = customElement.element[0];
+   uint stride = customStride.stride[0];
+   coopmatTypeA matA = coopmatTypeA(elementTypeA(0));
+   coopmatTypeB matB = coopmatTypeB(elementTypeB(0));
+   coopmatTypeC matC = coopmatTypeC(elementTypeC(0));
+
+   for (int i = 0; i < matA.length(); i++)
+   {
+      matA[i] = elementTypeA((0xA << 4) | i + 1);
+   }
+
+   for (int i = 0; i < matB.length(); i++)
+   {
+      matB[i] = elementTypeB((0xB << 4) | i + 1);
+   }
+
+   for (int i = 0; i < matC.length(); i++)
+   {
+      matC[i] = elementTypeC((0xC << 4) | i + 1);
+   }
+
+   barrier();
+
+   coopmatTypeD matD = coopMatMulAdd(matA, matB, matC);
+   barrier();
+
+   coopMatStore(matD, outputO.x, element, stride, gl_CooperativeMatrixLayoutRowMajor);
+}
--- scripts/generate_test_shaders.py
+++ scripts/generate_test_shaders.py
@@ -43,7 +43,64 @@ void main()
 """
 
 
-def generate_shader(
+SHADER_MULADD_TEMPLATE = """
+// compiler_extensions: VK_KHR_cooperative_matrix
+#version 450
+
+#extension GL_GOOGLE_include_directive : require
+#extension GL_EXT_shader_16bit_storage : require
+#extension GL_EXT_shader_explicit_arithmetic_types_float16 : require
+#extension GL_KHR_memory_scope_semantics : require
+#extension GL_EXT_shader_explicit_arithmetic_types : require
+#extension GL_KHR_shader_subgroup_basic : require
+#extension GL_KHR_cooperative_matrix : require
+
+<template configuration>
+
+layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z = 1) in;
+layout(set=0, binding=0) buffer Output { elementTypeD x[]; } outputO;
+layout(set=0, binding=1) buffer CustomStride { uint stride[]; } customStride;
+layout(set=0, binding=3) buffer CustomElement { uint element[]; } customElement;
+
+#define coopmatTypeA coopmat<elementTypeA, gl_ScopeSubgroup, M, K, gl_MatrixUseA>
+#define coopmatTypeB coopmat<elementTypeB, gl_ScopeSubgroup, K, N, gl_MatrixUseB>
+#define coopmatTypeC coopmat<elementTypeC, gl_ScopeSubgroup, M, N, gl_MatrixUseAccumulator>
+#define coopmatTypeD coopmat<elementTypeD, gl_ScopeSubgroup, M, N, gl_MatrixUseAccumulator>
+
+void main()
+{
+   uint element = customElement.element[0];
+   uint stride = customStride.stride[0];
+   coopmatTypeA matA = coopmatTypeA(elementTypeA(0));
+   coopmatTypeB matB = coopmatTypeB(elementTypeB(0));
+   coopmatTypeC matC = coopmatTypeC(elementTypeC(0));
+
+   for (int i = 0; i < matA.length(); i++)
+   {
+      matA[i] = elementTypeA((0xA << 4) | i + 1);
+   }
+
+   for (int i = 0; i < matB.length(); i++)
+   {
+      matB[i] = elementTypeB((0xB << 4) | i + 1);
+   }
+
+   for (int i = 0; i < matC.length(); i++)
+   {
+      matC[i] = elementTypeC((0xC << 4) | i + 1);
+   }
+
+   barrier();
+
+   coopmatTypeD matD = coopMatMulAdd(matA, matB, matC);
+   barrier();
+
+   coopMatStore(matD, outputO.x, element, stride, gl_CooperativeMatrixLayoutRowMajor);
+}
+"""
+
+
+def generate_shader_store(
     output_directory: Path,
     out: Dict[str, str],
     vk_type: str,
@@ -61,9 +118,41 @@ def generate_shader(
     config += f"#define MATRIX_USAGE {usage}\n"
     config += f"const int ROW = {row};\n"
     config += f"const int COL = {col};\n"
-    out[output_directory.joinpath(shader_name)] = (
-        f"// {shader_name}\n"
-        + SHADER_STORE_TEMPLATE.replace("<template configuration>", config)
+    out[
+        output_directory.joinpath(shader_name)
+    ] = f"// {shader_name}\n" + SHADER_STORE_TEMPLATE.replace(
+        "<template configuration>", config
+    )
+
+
+def generate_shader_muladd(
+    output_directory: Path,
+    out: Dict[str, str],
+    m: int,
+    n: int,
+    k: int,
+    vk_type_a: str,
+    vk_type_b: str,
+    vk_type_c: str,
+    vk_type_d: str,
+):
+    element_type_a = VK_TYPE_TO_GLSL_TYPE[vk_type_a]
+    element_type_b = VK_TYPE_TO_GLSL_TYPE[vk_type_b]
+    element_type_c = VK_TYPE_TO_GLSL_TYPE[vk_type_c]
+    element_type_d = VK_TYPE_TO_GLSL_TYPE[vk_type_d]
+    vk_types = f"{vk_type_a.lower()}_{vk_type_b.lower()}_{vk_type_c.lower()}_{vk_type_d.lower()}"
+    shader_name = f"matrix_muladd_{vk_types}_{m}x{n}x{k}.glsl"
+    config = f"#define elementTypeA {element_type_a}\n"
+    config += f"#define elementTypeB {element_type_b}\n"
+    config += f"#define elementTypeC {element_type_c}\n"
+    config += f"#define elementTypeD {element_type_d}\n"
+    config += f"const int M = {m};\n"
+    config += f"const int N = {n};\n"
+    config += f"const int K = {k};\n"
+    out[
+        output_directory.joinpath(shader_name)
+    ] = f"// {shader_name}\n" + SHADER_MULADD_TEMPLATE.replace(
+        "<template configuration>", config
     )
 
 
@@ -78,18 +167,31 @@ def append_shader_tests(
     n_size = entry["n_size"]
     k_size = entry["k_size"]
     matrix_name = f"{m_size}x{n_size}x{k_size}"
+
+    matrix_output_directory = output_directory.joinpath(matrix_name)
+
+    generate_shader_muladd(
+        matrix_output_directory,
+        out,
+        m_size,
+        n_size,
+        k_size,
+        a_type,
+        b_type,
+        c_type,
+        result_type,
+    )
+
     for layout_name, store_layout in [
         ("row", "gl_CooperativeMatrixLayoutRowMajor"),
         ("column", "gl_CooperativeMatrixLayoutColumnMajor"),
     ]:
-        final_output_directory = output_directory.joinpath(matrix_name).joinpath(
-            layout_name
-        )
+        final_output_directory = matrix_output_directory.joinpath(layout_name)
         final_output_directory.mkdir(parents=True, exist_ok=True)
 
         # MxNxK (A/B/C/D)
         # MxK (A)
-        generate_shader(
+        generate_shader_store(
             final_output_directory,
             out,
             a_type,
@@ -99,7 +201,7 @@ def append_shader_tests(
             store_layout,
         )
         # KxN (B)
-        generate_shader(
+        generate_shader_store(
             final_output_directory,
             out,
             b_type,
@@ -109,7 +211,7 @@ def append_shader_tests(
             store_layout,
         )
         # MxN (C)
-        generate_shader(
+        generate_shader_store(
             final_output_directory,
             out,
             c_type,
@@ -121,7 +223,7 @@ def append_shader_tests(
 
         # MxN (D)
         if c_type != result_type:
-            generate_shader(
+            generate_shader_store(
                 final_output_directory,
                 out,
                 result_type,
